4e2c75ae8250dd3a07355a31d42472e2f22c79ca
Su Mon Yee <sumonyee@op-j.jp>
2019-08-01 09:09:39 +0630

    create annotation routes

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,17 +3,31 @@
   //use App\Entity\Article;
   use Symfony\Component\HttpFoundation\Response;
   //use Symfony\Component\HttpFoundation\Request;
-  //use Symfony\Component\Routing\Annotation\Route;
+  use Symfony\Component\Routing\Annotation\Route;
+  use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
   class ArticleController {
     /**
-     * @Route("/", name="article_list")
+     * @Route("/")
      * @Method({"GET"})
      */
     public function index() {
       //$articles= $this->getDoctrine()->getRepository(Article::class)->findAll();
       //return $this->render('articles/index.html.twig', array('articles' => $articles));
-       return new Response('<html><body> Hello</body></html>') ;
+       return new Response('<html><body> Hello World!!!!</body></html>') ;
     }
+    /**
+     * @Route("/lucky/number")
+     */
+    public function number() {
+        //$articles= $this->getDoctrine()->getRepository(Article::class)->findAll();
+        //return $this->render('articles/index.html.twig', array('articles' => $articles));
+        $number = random_int(0, 100);
+
+        // return $this->render('lucky/number.html.twig', [
+        //     'number' => $number,
+        // ]);
+        return new Response('<html><body>'. $number .'</body></html>') ;
+      }
 
 }
